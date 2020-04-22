b148ad089bc897e9764034a8e6c4595820dac6fc
Helen Kent <helen8297@gmail.com>
2020-04-22 14:46:40 +0100

    updates

--- db/index.js
+++ db/index.js
@@ -8,6 +8,8 @@ const pool = new Pool({
   port: process.env.RDS_PORT,
 });
 
+// console.log(`this should be dbname ${RDS_DB_NAME}`);
+
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback);
