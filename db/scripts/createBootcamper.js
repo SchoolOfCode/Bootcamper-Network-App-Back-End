1ba7dd3a2484760d343024263c0f130e52391e16
Helen Kent <helen8297@gmail.com>
2020-04-22 10:04:57 +0100

    completed first script

--- db/scripts/createBootcamper.js
+++ db/scripts/createBootcamper.js
@@ -2,10 +2,10 @@ const { query } = require("../index");
 
 async function createContractsTable() {
   const res = await query(
-    `CREATE TABLE IF NOT EXISTS bootcampers (bootcamper_id SERIAL PRIMARY KEY, first_name TEXT, surname TEXT, profile TEXT, job_title TEXT, company_id INTEGER REFERENCES companies(company_id), salary TEXT, start_date DATE,  )`
+    `CREATE TABLE IF NOT EXISTS bootcampers (bootcamper_id SERIAL PRIMARY KEY, first_name TEXT, surname TEXT, profile TEXT, job_title TEXT, company_id INTEGER REFERENCES companies(company_id), salary TEXT, start_date DATE, previous_roles TEXT, cohort_num INTEGER, region TEXT, job_satisfaction INTEGER, new_job TEXT, twitter TEXT, github TEXT, portfolio TEXT, linkedin TEXT)`
   );
 
-  console.log(res);
+  console.log(`Log: create table script ${res}`);
 }
 
 createContractsTable();
