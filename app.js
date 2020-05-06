4e8863204f216a864baa417263972da292946113
nevillejodie <jlneville92@gmail.com>
2020-05-06 16:03:25 +0100

    sockets

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const bootcampRouter = require("./routes/bootcampers.js");
 const companyRouter = require("./routes/companies");
 const eventRouter = require("./routes/events");
+/* const messageRouter = require("./routes/messages") */
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.use((req, res, next) => {
   console.log(`${req.method} request received to ${req.url}"`);
@@ -17,10 +20,17 @@ app.get("/health", (req, res) => {
   res.status(200).send("Hello I'm working!");
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 app.use(bootcampRouter);
 app.use(companyRouter);
 app.use(eventRouter);
-
-app.listen(PORT, () => {
+/* app.use(messageRouter) */
+http.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
---
+++ routes/messages.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
+module.exports = router;
