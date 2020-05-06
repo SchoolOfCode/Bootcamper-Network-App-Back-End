b37d882fe2d9f82aa91fd8447d8fee96f684b1fc
Helen Kent <helen8297@gmail.com>
2020-05-06 22:53:35 +0100

    socket update for backend

--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router = express.Router();
-var io = require('socket.io')(http);
+// const express = require("express");
+// const router = express.Router();
+// var io = require('socket.io')(http);
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//   });
 
-module.exports = router;
+// module.exports = router;
