15d7860b083cbf5c20f24c06dfad6eda09fceef7
Helen Kent <helen8297@gmail.com>
2020-05-11 22:30:18 +0100

    setting up queries for messages on back end - not all linked up yet

--- app.js
+++ app.js
@@ -21,14 +21,38 @@ app.get("/health", (req, res) => {
   res.status(200).send("Hello I'm working!");
 });
 
+async function sendMessageToDb({ bootcamper_id, message }) {
+  const data = await query(
+    `INSERT INTO messages(bootcamper_id, message)
+       values ($1, $2) RETURNING bootcamper_id`,
+    [bootcamper_id, message]
+  );
+  console.log(`POST: post message Results:`, data.rows);
+  return data.rows;
+}
+
+async function getMessagesFromDb({ bootcamper_id, message }) {
+  const data = await query(
+    `SELECT messages.bootcamper_id, first_name,
+    surname,
+    photo_url,
+    message,
+    sent
+    FROM bootcampers LEFT JOIN messages ON bootcampers.bootcamper_id = messages.bootcamper_id`
+  );
+  console.log(`Get: allmessages Results:`, data.rows);
+  return data.rows;
+}
+
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
   socket.on("chatMessage", (myMessage) => {
+    sendMessageToDb(myMessage);
     console.log("message received from front end:", myMessage);
-    io.emit("chatMessage", myMessage);
+    io.emit("chatMessage", myMessage); //helen up to here - how to send all the messages back, maybe send data from the get instead of 'mymessage'
   });
 });
 
